bbcdfdc84b3f6de10624bacf67c66007d316991d
ryanmccarthypdx <ryanmccarthypdx@gmail.com>
2015-01-22 15:15:51 -0800

    Add spendings.rb functionality

--- lib/spending.rb
+++ lib/spending.rb
@@ -1,22 +1,34 @@
-class Spending
 
-  attr_reader(:business_id, :expense_id, :id)
+
+class Spending
+  attr_reader(:id, :expenses_id, :categories_id)
 
   define_method(:initialize) do |attributes|
     @id = attributes.fetch(:id)
-    @business_id = attributes.fetch(:business_id)
-    @expense_id = attributes.fetch(:expense_id)
+    @categories_id = attributes.fetch(:categories_id)
+    @expenses_id = attributes.fetch(:expenses_id)
   end
 
   define_singleton_method(:all) do
     returned_spendings = DB.exec("SELECT * FROM spendings;")
     spendings = []
     returned_spendings.each() do |spending|
-      @business_id = spending.fetch("business_id").to_i()
-      @expense_id = spending.fetch("expense_id").to_i()
-      spendings.push(Spending.new({:business_id => @business_id, :expense_id => @expense_id}))
+      @categories_id = spending.fetch("categories_id").to_i()
+      @expenses_id = spending.fetch("expenses_id").to_i()
+      @id = spending.fetch("id").to_i()
+      spendings.push(Spending.new({:id => @id, :categories_id => @categories_id, :expenses_id => @expenses_id}))
     end
     spendings
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO spendings (categories_id, expenses_id) VALUES (#{@categories_id}, #{@expenses_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_spendings|
+    self.expenses_id().==(another_spendings.expenses_id()).&(self.id().==(another_spendings.id()))
+  end
+
 
 end
