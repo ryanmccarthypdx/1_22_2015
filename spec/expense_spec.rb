211bec8201ecb605754efb7b6657d10798f1f85f
ryanmccarthypdx <ryanmccarthypdx@gmail.com>
2015-01-22 16:46:52 -0800

    End of hte day add

--- spec/expense_spec.rb
+++ spec/expense_spec.rb
@@ -55,4 +55,24 @@ describe(Expense) do
        expect(Expense.find(tid).business_id()).to(eq(2))  #this is awesome: you can do multiple expects, no problem
      end
    end
-end
+
+   describe('#categorize') do
+     it('takes an expense and categorizes it') do
+       test_expense = Expense.new({ :id => nil, :name => "condoms", :amount => 6.99, :date => "2015-01-22",:business_id => 1})
+       test_category1 = Category.new({:id => nil, :name => "Pleasure"})
+       test_category2 = Category.new({:id => nil, :name => "Business"})
+       test_category.save()
+       test_expense.save()
+       test_expense_id = test_expense.id()
+       tc1i = test_category1.id()
+       tc2i = test_category2.id()
+       test_expenses.categorize([tc1i, tc2i])
+       #expect(Expense.regurgitate(test_expense_id)).to(eq([test_category1, test_category2]))
+       all_spendings = Spendings.all()
+       output = []
+       all_spendings.each() do |spending|
+         output.push(spending.category_id())
+       end
+       expect(output).to(eq[tc1i, tc2i])
+     end
+   end
